1e2a73bb9fef3e9d0b942c108a5f2df9ef37413d
Sudish kumar <sudishkumar.edugaon@gmail.com>
2025-01-13 17:59:34 +0530

    text setting and getting

--- app/src/main/java/com/edugaon/basicwidgets/MainActivity.kt
+++ app/src/main/java/com/edugaon/basicwidgets/MainActivity.kt
@@ -20,15 +20,28 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-        val clickMeButton = findViewById<Button>(R.id.clickMeButton) // connecting clickMeButton to clickMeButton variable to performing click event
-        val clickMeText = findViewById<TextView>(R.id.clickMeText) // connecting clickMeText to clickMeText variable to performing click event
+        val submitNowButton =
+            findViewById<Button>(R.id.submitNowButton) // connecting clickMeButton to clickMeButton variable to performing click event
+        val submittedText =
+            findViewById<TextView>(R.id.submittedText) // connecting clickMeText to clickMeText variable to performing click event
+        val nameEditText =
+            findViewById<TextView>(R.id.enterNameEditText) // connecting clickMeText to clickMeText variable to performing click event
 
-        clickMeButton.setOnClickListener { // adding click event
-            Toast.makeText(this, "You clicked on Button", Toast.LENGTH_SHORT).show() // showing short time message
-        }
+        /*
+          val nameText = nameEditText.text //text getting
 
-        clickMeText.setOnClickListener { // adding click event
-            Toast.makeText(this, "You clicked on Text", Toast.LENGTH_SHORT).show() // showing short time message
+          Toast.makeText(this, "$nameText", Toast.LENGTH_SHORT).show()
+
+          nameEditText.setText("Hello text is inserted") //text setting
+          val nameText1 = nameEditText.text //text getting
+        */
+
+        submitNowButton.setOnClickListener { // adding click event
+            val nameText = nameEditText.text //text gettingf
+
+            Toast.makeText(this, "$nameText", Toast.LENGTH_SHORT).show()
+
+            submittedText.text = nameText //setting text
         }
     }
 }
