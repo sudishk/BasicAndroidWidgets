432add08120f44d0dc8b666459ebcf6852c5ed1f
Sudish kumar <sudishkumar.edugaon@gmail.com>
2025-01-13 18:02:30 +0530

    Removed extra codes

--- app/src/main/java/com/edugaon/basicwidgets/MainActivity.kt
+++ app/src/main/java/com/edugaon/basicwidgets/MainActivity.kt
@@ -20,28 +20,15 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-        val submitNowButton =
-            findViewById<Button>(R.id.submitNowButton) // connecting clickMeButton to clickMeButton variable to performing click event
-        val submittedText =
-            findViewById<TextView>(R.id.submittedText) // connecting clickMeText to clickMeText variable to performing click event
-        val nameEditText =
-            findViewById<TextView>(R.id.enterNameEditText) // connecting clickMeText to clickMeText variable to performing click event
+        val clickMeButton = findViewById<Button>(R.id.clickMeButton) // connecting clickMeButton to clickMeButton variable to performing click event
+        val clickMeText = findViewById<TextView>(R.id.clickMeText) // connecting clickMeText to clickMeText variable to performing click event
 
-        /*
-          val nameText = nameEditText.text //text getting
+        clickMeButton.setOnClickListener { // adding click event
+            Toast.makeText(this, "You clicked on Button", Toast.LENGTH_SHORT).show() // showing short time message
+        }
 
-          Toast.makeText(this, "$nameText", Toast.LENGTH_SHORT).show()
-
-          nameEditText.setText("Hello text is inserted") //text setting
-          val nameText1 = nameEditText.text //text getting
-        */
-
-        submitNowButton.setOnClickListener { // adding click event
-            val nameText = nameEditText.text //text gettingf
-
-            Toast.makeText(this, "$nameText", Toast.LENGTH_SHORT).show()
-
-            submittedText.text = nameText //setting text
+        clickMeText.setOnClickListener { // adding click event
+            Toast.makeText(this, "You clicked on Text", Toast.LENGTH_SHORT).show() // showing short time message
         }
     }
 }
